28225826b9868dad396c8efdf7134cc6d8faf727
ghofrandayyat <ghofrandayyat@gmail.com>
2021-04-04 14:13:30 +0300

    done

--- server.js
+++ server.js
@@ -1,5 +1,40 @@
 'use strict';
+require('dotenv').config();
 
 
+// Application Dependencies
+const express = require('express');
+const superagent  = require('superagent');
+const cors = require('cors');
 
-let ejs = require('ejs');
+
+// Application Setup
+const app = express();
+const PORT = process.env.PORT || 3434;
+
+
+// Application Middleware
+app.use(express.static('./public'));
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+
+
+// Set the view engine for server-side templating
+app.set('view engine','ejs');
+app.get('/hello',homePage);
+// app.get('/',homePage);
+
+
+function homePage(req,res){
+  res.render('pages/index');
+}
+
+
+// Catch-all-errors
+app.get('*',(req,res)=>{
+  res.status(404).send('something went wrong');
+});
+
+app.listen(PORT,()=>{
+  console.log(`listening on PORT ${PORT}`);
+});

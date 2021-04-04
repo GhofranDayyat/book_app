522c33f40a51eab6b8db361a2a97fc2c5c62fcda
ghofrandayyat <ghofrandayyat@gmail.com>
2021-04-04 23:31:37 +0300

    done

--- server.js
+++ server.js
@@ -21,13 +21,57 @@ app.use(express.urlencoded({ extended: true }));
 
 // Set the view engine for server-side templating
 app.set('view engine','ejs');
-app.get('/hello',homePage);
-// app.get('/',homePage);
+// app.get('/hello',homePage);
+app.get( `/searches/new`,showForm);
+app.get('/',homePage);
+app.post('/searches',createSearch);
+
 
 
 function homePage(req,res){
   res.render('pages/index');
 }
+
+function showForm(req,res){
+  res.render('pages/searches/new.ejs');
+}
+
+
+//Constrecter
+function Book(info) {
+  this.title = info.title;
+  this.author = info.authors || 'Authors';
+  this.description = info.description;
+  this.thumbnail = 'https://i.imgur.com/J5LVHEL.jpg';
+  // this.thumbnail = (info.imageLinks.thumbnail)? info.imageLinks.thumbnail : 'https://i7.uihere.com/icons/829/139/596/thumbnail-caefd2ba7467a68807121ca84628f1eb.png';
+}
+
+function createSearch(req,res){
+
+  let url = 'https://www.googleapis.com/books/v1/volumes';
+  console.log(req.body);
+  const searchBy = req.body.searchBy;
+  const searchValue = req.body.search;
+  const query = {};
+  if (searchBy === 'title') {
+    query['q'] = `intitle:'${searchValue}'`;
+
+  } else if (searchBy === 'author') {
+    query['q'] = `inauthor:'${searchValue}'`;
+  }
+  console.log(searchValue);
+  // send the URL to the servers API
+  console.log(query);
+  superagent.get(url).query(query).then(search => {
+    return search.body.items.map(searchBook => new Book(searchBook.volumeInfo));
+  }).then(results => {
+    res.render('pages/searches/show', { searchResults: results });
+  }).catch((error) => {
+    console.error('ERROR', error);
+    res.status(500).render('pages/error');
+  });
+}
+
 
 
 // Catch-all-errors
